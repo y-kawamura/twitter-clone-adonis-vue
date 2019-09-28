5bf720f6233e5aa5824f38fff4fcbfb72c2f3b70
y-kawamura <tetora.y@gmail.com>
2019-09-29 08:36:12 +0900

    Create models

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,9 +7,13 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
+      table.string('name').notNullable()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('location').notNullable()
+      table.string('website_url').notNullable()
+      table.string('bio').notNullable()
       table.timestamps()
     })
   }
